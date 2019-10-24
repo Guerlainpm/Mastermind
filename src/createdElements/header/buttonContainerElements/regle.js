ec3956a42583d195ddfb4534cd0fc0a68c5253fe
Guerlainpm <46000981+Guerlainpm@users.noreply.github.com>
2019-10-24 16:24:05 +0200

    finis

--- src/createdElements/header/buttonContainerElements/regle.js
+++ src/createdElements/header/buttonContainerElements/regle.js
@@ -4,7 +4,7 @@ class Regle extends React.Component{
   render(){
 
     return(
-      <a href="https://www.regles-de-jeux.com/regle-du-mastermind/"><button type='button' className="bg-color-white color-red-5 bd-color-red-5">Règle</button></a>
+      <a href="https://www.regles-de-jeux.com/regle-du-mastermind/" target='blank'><button type='button' className="bg-color-white color-red-5 bd-color-red-5">Règle</button></a>
     );
   }
 }
